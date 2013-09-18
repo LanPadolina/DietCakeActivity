e218dd896ab4213ff9bb91943e82ae2907bc507b
lavander-sky <n.padolina@klab.com>
2013-09-18 10:32:19 +0800

    Uploading board directory files

---
+++ board/app/models/hello.php
@@ -0,0 +1,8 @@
+<?php
+class Hello extends AppModel
+{
+    public static function getMessage()
+    {
+        return 'HELLO KLAB CYSCORPIONS!';
+    }
+}
---
+++ board/app/views/hello/index.php
@@ -0,0 +1,4 @@
+<?php $title = 'Hello' ?>
+
+<h1><?php eh($message) ?></h1>
+<p>Use this space as a way to quick start any new project.</p>
